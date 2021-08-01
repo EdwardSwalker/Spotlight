3a8af85d27fb4cb949127438da0fac416525254b
Edward Swalker <edwardtang0611@gmail.com>
2021-08-01 21:46:18 +0800

    title and section 1

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+      "./dist/**/*.html"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      productSans: "'Product Sans', serif",
+      jf: "'jf', serif"
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
